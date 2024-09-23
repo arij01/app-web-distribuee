f4155ef7bd884186cad49a3d2a5c4a8bdf2d3f90
arij <m'hiri.arij@esprit.tn>
2024-09-23 10:54:12 +0100

    added discovery client

--- src/main/java/tn/esprit/candidat/CandidatApplication.java
+++ src/main/java/tn/esprit/candidat/CandidatApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CandidatApplication {
     public static void main(String[] args) { SpringApplication.run(CandidatApplication.class, args);
     }
